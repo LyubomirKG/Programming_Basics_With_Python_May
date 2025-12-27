b2611324085a2897ed1295824b91ab38cbf33701
Lyubomir Georgiev <lyubomir.k.georgiev@gmail.com>
2025-12-27 16:35:06 +0200

    Add radians to degrees conversion script

---
+++ First_steps_in_Coding_Exercise/radians_to_degrees.py
@@ -0,0 +1,10 @@
+import math
+
+# Input: value in radians
+radians = float(input())
+
+# Conversion formula: degrees = radians * 180 / pi
+degrees = radians * 180 / math.pi
+
+# Output the result in degrees
+print(degrees)
