62936a9e4077d6031b6df1810dcf0ee9c084409e
Lyubomir Georgiev <lyubomir.k.georgiev@gmail.com>
2025-12-27 15:50:33 +0200

    Check number parity using modulo operator

---
+++ Conditional_Statements_Lab/even_or_odd.py
@@ -0,0 +1,9 @@
+# Input: integer number
+number = int(input())
+
+# A number is even if the remainder when divided by 2 is 0
+if number % 2 == 0:
+    print('even')
+else:
+    # Otherwise, the number is odd
+    print('odd')
